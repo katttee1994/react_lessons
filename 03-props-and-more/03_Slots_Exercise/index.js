3bf280bf2901e51596ee30a70c25a1a259e501a2
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-03-28 22:10:49 +0200

    SlotMachine component created

---
+++ 03-props-and-more/03_Slots_Exercise/index.js
@@ -0,0 +1,26 @@
+class App extends React.Component {
+  render() {
+    return (
+    <div>
+
+      <SlotMachine
+        s1="y"
+        s2="y"
+        s3="z"
+      />
+      <SlotMachine
+      s1="y"
+      s2="y"
+      s3="y"
+      />
+      <SlotMachine
+      s1="x"
+      s2="y"
+      s3="z"
+      />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
