79f10332eac27a504f6397f7eab44e5dcc5b83ab
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-04-03 19:42:45 +0200

    format files

--- 03-props-and-more/01_Hello_Props/Hello.js
+++ 03-props-and-more/01_Hello_Props/Hello.js
@@ -1,13 +1,16 @@
 class Hello extends React.Component {
-    render() {
-        // repeat sluzy do powtorzen. w tym przypadku 4 razy zostanie powtorzony "!"
-        let bangs = "!".repeat(this.props.bangs); 
+  render() {
+    // repeat sluzy do powtorzen. w tym przypadku 4 razy zostanie powtorzony "!"
+    let bangs = "!".repeat(this.props.bangs);
     //    nie mozemy modyfikowac, ani dodawac nic do properties. Nie mozna tego bezposrednio zrobic. They are immutable
-        return (
-        <div>
-            <p>Hi {this.props.to} from {this.props.from}{bangs}</p>
-            <img src={this.props.img} />
-        </div>
-        );
-    }
+    return (
+      <div>
+        <p>
+          Hi {this.props.to} from {this.props.from}
+          {bangs}
+        </p>
+        <img src={this.props.img} />
+      </div>
+    );
+  }
 }
--- 03-props-and-more/04_JSX_Loops/Friend.js
+++ 03-props-and-more/04_JSX_Loops/Friend.js
@@ -1,21 +1,22 @@
 class Friend extends React.Component {
-    render() {
-      // poniżej jest object destructuring assignment. Dzięki takiemu zapisowi, 
-      // nie musimy już ponizej wpisywac np. this.props.name tylko same name piszemy
-      const {name, hobbies} = this.props;
-      return (
+  render() {
+    // poniżej jest object destructuring assignment. Dzięki takiemu zapisowi,
+    // nie musimy już ponizej wpisywac np. this.props.name tylko same name piszemy
+    const { name, hobbies } = this.props;
+    return (
       <div>
         <h1>{name}</h1>
         <ul>
-         
           {/* {<li>Singing</li>
-          <li>Dancing</li>} */} 
+          <li>Dancing</li>} */}
           {/* to co powyzej tworzy nam liste z podpunktami (kropkami)mozemy rowniez zapisac za pomocą map */}
-          {hobbies.map(hobbies => <li>{hobbies}</li>)}
+          {hobbies.map((hobbies) => (
+            <li>{hobbies}</li>
+          ))}
           {/* w kodzie powyzej po znaku => wspisujemy cokolwiek. 
           Moze to byc np tylko h i potem to samo wpisyjemy w <li>{hobbies}</li>  */}
         </ul>
       </div>
-      )
-    }
+    );
   }
+}
