dffceddb35e068101a1af9f57a0991e83bf6c659
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-04-17 20:41:37 +0200

    pokecard template created

---
+++ 05-pokedex-project/pokecard/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Pokecard from "./Pokecard";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Pokecard id={4} name="Charmander" type="fire" exp={62} />
+      </div>
+    );
+  }
+}
+
+export default App;
