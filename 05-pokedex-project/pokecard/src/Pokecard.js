e71969be4df55023c71ef33d0ea06a2d3f339ed6
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-04-24 18:26:16 +0200

    Small pokemon game with cards

--- 05-pokedex-project/pokecard/src/Pokecard.js
+++ 05-pokedex-project/pokecard/src/Pokecard.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import './Pokecard.css';
+import "./Pokecard.css";
 // const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
-const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
 
 // This finction pad two three takes a number. If the number is grater then 999,
 // then we don't pad it period and we just return the number. `00${number} - here
@@ -9,7 +9,7 @@ const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
 // and returning the number. Czyli jak mamy liczbe 23 dodajemy do niej 00, wychodzi 0023
 // i ucinamy i zostaja nam 3 ostatnie cyfry i wtedy mamy id pokemona
 
-let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3): number);
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 class Pokecard extends Component {
   render() {
@@ -19,7 +19,9 @@ class Pokecard extends Component {
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
-        <img src={imgSrc} alt={this.props.name}/>
+        <div className="Pokecard-image">
+          <img src={imgSrc} alt={this.props.name} />
+        </div>
         {/* alt attribute specifies an alternate text for an image, if the image cannot be displayed. */}
         <div className="Pokecard-data">Type: {this.props.type}</div>
         <div className="Pokecard-data">EXP: {this.props.exp}</div>
