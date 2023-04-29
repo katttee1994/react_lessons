f038ae5bb2c7c080c4e1cca6954b287325c05561
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-04-29 17:06:17 +0200

    state() and state(props)

--- 06-react-state/state-demo/src/App.js
+++ 06-react-state/state-demo/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import Game from "./Game";
+import Demo from "./Demo";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Game />
+      <div className="App">
+        <Game color="purple" />
+        <Demo animal="Bobcat" food="Pineapple" />
+        {/* Można by to też rozpisać jak
+        new Demo({animal: "Bobcat" food: "Pineapple"}) */}
       </div>
     );
   }
