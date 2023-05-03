1d4adee688d3574cdf433dc55a99e1ffc7b3de2a
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-05-03 13:28:56 +0200

    Changing state last lesson

--- 06-react-state/state-demo/src/App.js
+++ 06-react-state/state-demo/src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
-import Game from "./Game";
-import Demo from "./Demo";
-import Rando from "./Rando";
+import BrokenClick from "./BrokenClick";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* Rando ma generowac losową liczbe. Ponizej w maxNum
-        wspisujemy jaka ma byc maksymalna liczba */}
-        <Rando maxNum={7} />
+        <BrokenClick />
       </div>
     );
   }
