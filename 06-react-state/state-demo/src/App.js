7a98abd2bc233fc04943f06ff122f2e34b848da0
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-05-04 17:31:41 +0200

    Easy clicker

--- 06-react-state/state-demo/src/App.js
+++ 06-react-state/state-demo/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import BrokenClick from "./BrokenClick";
+import Clicker from "./Clicker";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrokenClick />
+        <Clicker />
       </div>
     );
   }
---
+++ 06-react-state/state-demo/src/Clicker.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class Clicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { num: 1 };
+    this.genRandom = this.genRandom.bind(this);
+  }
+  genRandom() {
+    // pick random number 1-10
+    let rand = Math.floor(Math.random() * 10) + 1;
+    // Jeżeli nie dodamy tam +1 to wtedy będą to liczby od 1-9
+    // update state with new rand
+    this.setState({ num: rand });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Number is: {this.state.num}</h1>
+        {this.state.num === 7 ? (
+          <h2>YOU WIN!</h2>
+        ) : (
+          <button onClick={this.genRandom}>Random Number</button>
+        )}
+        {/* drugi sposób */}
+        {/* {this.state.num === 7 && <h2>YOU WIN!</h2>}
+        {this.state.num !== 7 && (
+          <button onClick={this.genRandom}>Random Number</button>
+        )} */}
+      </div>
+    );
+  }
+}
+
+export default Clicker;
