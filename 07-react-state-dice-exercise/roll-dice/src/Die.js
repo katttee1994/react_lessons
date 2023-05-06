428e20564348ede88d6abd0b645f5d2334e047e8
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-05-06 18:58:37 +0200

    Last part of Dice Rolls - animation

--- 07-react-state-dice-exercise/roll-dice/src/Die.js
+++ 07-react-state-dice-exercise/roll-dice/src/Die.js
@@ -3,7 +3,16 @@ import "./Die.css";
 
 class Die extends Component {
   render() {
-    return <i className={`Die fas fa-dice-${this.props.face}`}></i>;
+    return (
+      <i
+        className={`Die fas fa-dice-${this.props.face} ${
+          this.props.rolling ? "shaking" : ""
+        }`}
+        // mozna to tez zapisac
+        // className={`Die fas fa-dice-${this.props.face} ${
+        // this.props.rolling && "shaking"}`}
+      ></i>
+    );
   }
 }
 //  można to też zapisac tak jak ponizej
--- 07-react-state-dice-exercise/roll-dice/src/RollDice.js
+++ 07-react-state-dice-exercise/roll-dice/src/RollDice.js
@@ -8,7 +8,7 @@ class RollDice extends Component {
   };
   constructor(props) {
     super(props);
-    this.state = { die1: "one", die2: "one" };
+    this.state = { die1: "one", die2: "one", rolling: false };
     this.roll = this.roll.bind(this);
   }
   roll() {
@@ -18,17 +18,28 @@ class RollDice extends Component {
     const newDie2 =
       this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
     //set state with new rolls
-    this.setState({ die1: newDie1, die2: newDie2 });
+    this.setState({ die1: newDie1, die2: newDie2, rolling: true });
+
+    // wait one second (1000 miliseconds), then set rolling to false
+    setTimeout(() => {
+      this.setState({ rolling: false });
+    }, 1000);
   }
   render() {
     return (
       <div className="RollDice">
         <div className="RollDice-container">
-          <Die face={this.state.die1} />
-          <Die face={this.state.die2} />
+          <Die face={this.state.die1} rolling={this.state.rolling} />
+          <Die face={this.state.die2} rolling={this.state.rolling} />
         </div>
-
-        <button onClick={this.roll}>Roll Dice!</button>
+        {/* disabled={this.state.rolling}> ta część kodu powoduje blokadę button tylko gdy rolling = true,
+gdybysmy wpisali disabled={true}> to przycisk byłby wyłączony całkowicie co oznacza, ze nie moglibysmy
+go klikac*/}
+        <button onClick={this.roll} disabled={this.state.rolling}>
+          {/* kod ponizej mowi, że jezeli rolling is true to 
+        będzie Rolling... a jeżeli false to Roll Dice! */}
+          {this.state.rolling ? "Rolling..." : "Roll Dice!"}
+        </button>
       </div>
     );
   }
