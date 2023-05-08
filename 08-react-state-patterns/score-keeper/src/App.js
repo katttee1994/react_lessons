5bec3db245878041c2ade138176f98812425b844
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-05-08 19:35:23 +0200

    Generowanie przypadkowych ikonek - byl problem z ich wyswietleniem. Mutating state

--- 08-react-state-patterns/score-keeper/src/App.js
+++ 08-react-state-patterns/score-keeper/src/App.js
@@ -1,12 +1,15 @@
+import React, { Component } from "react";
+import IconList from "./IconList";
 import "./App.css";
-import ScoreKeeper from "./ScoreKeeper";
 
-function App() {
-  return (
-    <div className="App">
-      <ScoreKeeper />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <IconList />
+      </div>
+    );
+  }
 }
 
 export default App;
