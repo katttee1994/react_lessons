964ca75fb27ef05a648ae829029301bcc8665d59
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-05-10 18:23:19 +0200

    Second part of lottery app

--- 08-react-state-patterns/score-keeper/src/App.js
+++ 08-react-state-patterns/score-keeper/src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import Ball from "./Ball";
+import Lottery from "./Lottery";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Ball num={17} />
-        <Ball num={33} />
-        <Ball num={64} />
-        <Ball num={12} />
+        <Lottery />
       </div>
     );
   }
