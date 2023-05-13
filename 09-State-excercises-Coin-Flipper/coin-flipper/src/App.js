87d44564bea1b4ecdf0863c63775bff5bf7cb9c7
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-05-13 19:57:30 +0200

    second part of making coin flipper. Adding coin img, changing sides of a coin

--- 09-State-excercises-Coin-Flipper/coin-flipper/src/App.js
+++ 09-State-excercises-Coin-Flipper/coin-flipper/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import CoinContainer from "./CoinContainer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <CoinContainer />
+    </div>
+  );
 }
 
 export default App;
