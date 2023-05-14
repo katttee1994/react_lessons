f83b74e4dcf2f2a721120e0538b7cbff16a0c714
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-05-14 13:03:53 +0200

    fucking ';' removed

--- 09-State-excercises-Coin-Flipper/coin-flipper/src/Coin.js
+++ 09-State-excercises-Coin-Flipper/coin-flipper/src/Coin.js
@@ -5,7 +5,7 @@ class Coin extends Component {
   render() {
     return (
       <div className="Coin">
-        <img src={this.props.info.imgSrc} alt={this.props.info.side} />;
+        <img src={this.props.info.imgSrc} alt={this.props.info.side} />
       </div>
     );
   }
