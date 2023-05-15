91188ad8ec071600edd7ed9c2dd77bafa2fc4422
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-05-15 17:38:03 +0200

    first part of color boxes

---
+++ 09-State-excercises-Coin-Flipper/color-boxes/src/Box.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { choice } from "./helpers";
+import "./Box.css";
+
+class Box extends Component {
+  static defaultProps = {
+    allColors: ["purple", "magenta", "lilac", "pink"],
+  };
+  constructor(props) {
+    super(props);
+    this.state = { color: choice(this.props.allColors) };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    console.log("dupa");
+  }
+  render() {
+    return (
+      <div
+        className="Box"
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.handleClick}
+      />
+    );
+  }
+}
+
+export default Box;
