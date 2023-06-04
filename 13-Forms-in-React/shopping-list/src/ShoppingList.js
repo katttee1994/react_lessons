36ae8f75b55bf8238f57639ba6fca85b7cc51481
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-06-04 20:00:12 +0200

    New small project - Shopping list. Adding name and quantity

---
+++ 13-Forms-in-React/shopping-list/src/ShoppingList.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import ShoppingListForm from "./ShoppingListForm";
+
+class ShoppingList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        { name: "Milk", qty: "2 gallons" },
+        { name: "Bread", qty: "2 loaves" },
+      ],
+    };
+    this.addItem = this.addItem.bind(this);
+  }
+  //   kod ponizej mowi: wez nowe dane i wstaw je do items powyzej
+  addItem(item) {
+    this.setState((state) => ({
+      items: [...state.items, item],
+    }));
+  }
+  renderItems() {
+    return (
+      <ul>
+        {this.state.items.map((item) => (
+          <li>
+            {item.name}:{item.qty}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  render() {
+    return (
+      <div>
+        <h1>Shopping List</h1>
+        {this.renderItems()}
+        <ShoppingListForm addItem={this.addItem} />
+      </div>
+    );
+  }
+}
+
+export default ShoppingList;
