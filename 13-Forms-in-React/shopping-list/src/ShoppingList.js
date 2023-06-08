d3caaa938803da4f2112844eae26af9cfe6c6c69
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-06-08 12:57:52 +0200

    Adding unique key by using UUID Libary

--- 13-Forms-in-React/shopping-list/src/ShoppingList.js
+++ 13-Forms-in-React/shopping-list/src/ShoppingList.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
 import ShoppingListForm from "./ShoppingListForm";
-
+import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid"; to pozwala nam na dodawanie unikalnego id do elementow,
+// najpierw trzeba w folderze z projektem zainstalowac to: npm install uuid, potem
+// najlepiej znalezc to w folderze i poczytac readme.
 class ShoppingList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [
-        { name: "Milk", qty: "2 gallons" },
-        { name: "Bread", qty: "2 loaves" },
+        { name: "Milk", qty: "2 gallons", id: uuidv4() },
+        { name: "Bread", qty: "2 loaves", id: uuidv4() },
       ],
     };
     this.addItem = this.addItem.bind(this);
   }
   //   kod ponizej mowi: wez nowe dane i wstaw je do items powyzej
   addItem(item) {
+    let newItem = { ...item, id: uuidv4() };
     this.setState((state) => ({
-      items: [...state.items, item],
+      items: [...state.items, newItem],
     }));
   }
   renderItems() {
