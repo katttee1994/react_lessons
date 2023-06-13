166013ecba09f78b732d61fb188d78acf4a6c580
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-06-13 17:18:44 +0200

    Last part of Box maker by creating removing color button

--- 14-Forms-exercise/color-box-maker/src/BoxList.js
+++ 14-Forms-exercise/color-box-maker/src/BoxList.js
@@ -5,8 +5,13 @@ import NewBoxForm from "./NewBoxForm";
 class BoxList extends Component {
   constructor(props) {
     super(props);
-    this.state = { boxes: [{ width: 10, height: 10, color: "orange" }] };
+    this.state = { boxes: [] };
     this.create = this.create.bind(this);
+  }
+  remove(id) {
+    this.setState({
+      boxes: this.state.boxes.filter((box) => box.id !== id),
+    });
   }
   create(newBox) {
     this.setState({
@@ -15,7 +20,14 @@ class BoxList extends Component {
   }
   render() {
     const boxes = this.state.boxes.map((box) => (
-      <Box width={box.width} height={box.height} color={box.color} />
+      <Box
+        key={box.id}
+        id={box.id}
+        width={box.width}
+        height={box.height}
+        color={box.color}
+        removeBox={() => this.remove(box.id)}
+      />
     ));
     return (
       <div>
--- 14-Forms-exercise/color-box-maker/src/NewBoxForm.js
+++ 14-Forms-exercise/color-box-maker/src/NewBoxForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 class NewBoxForm extends Component {
   constructor(props) {
@@ -15,7 +16,8 @@ class NewBoxForm extends Component {
   handleSubmit(evt) {
     //  evt.preventDefault(); to dodajemy zeby nam nie odswiezalo strony automatycznie
     evt.preventDefault();
-    this.props.createBox(this.state);
+    const newBox = { ...this.state, id: uuidv4() };
+    this.props.createBox(newBox);
     // to ponizej powoduje ze jak wspisujemy cos w formularzu to po
     // kliknieciu dodaj, formularz robi sie znowu pusty
     this.setState({
