57fda4595105779b0eb998e38365487f9746f4df
katttee1994 <120603509+katttee1994@users.noreply.github.com>
2023-06-16 19:23:31 +0200

    First and second part of making todo list. Adding things and removing them

---
+++ 15-todo-list-project/todo-list/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+import TodoList from "./TodoList";
+
+function App() {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
